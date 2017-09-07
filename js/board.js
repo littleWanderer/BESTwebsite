2fb89db100a254ca5e4f798afbcefcd03fc41eac
unknown <krstta@gmail.com>
2017-09-07 19:29:43 +0200

    about us

---
+++ js/board.js
@@ -0,0 +1,186 @@
+/* ---------------   BOARD 16/17 ----------*/
+
+
+function myFunction()
+{
+var img = document.getElementById("image");
+img.src="img/macka.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image1");
+img.src="img/macka.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image2");
+img.src="img/macka.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image3");
+img.src="img/macka.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image4");
+img.src="img/macka.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image5");
+img.src="img/macka.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+
+}
+
+
+
+
+/* ---------------   BOARD 15/16 ----------*/
+
+
+function myFunction1()
+{
+var img = document.getElementById("image");
+img.src="img/macka1.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image1");
+img.src="img/macka1.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image2");
+img.src="img/macka1.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image3");
+img.src="img/macka1.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image4");
+img.src="img/macka1.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image5");
+img.src="img/macka1.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+}
+
+
+
+
+/* ---------------   BOARD 14/15 ----------*/
+
+
+function myFunction2()
+{
+var img = document.getElementById("image");
+img.src="img/macka2.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image1");
+img.src="img/macka2.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image2");
+img.src="img/macka2.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image3");
+img.src="img/macka2.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image4");
+img.src="img/macka2.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image5");
+img.src="img/macka2.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+}
+
+
+/* ---------------   BOARD 13/14 ----------*/
+
+
+function myFunction3()
+{
+var img = document.getElementById("image");
+img.src="img/macka3.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+document.getElementById("li_n").href = "https://www.google.rs/";
+
+
+var img = document.getElementById("image1");
+img.src="img/macka3.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image2");
+img.src="img/macka3.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image3");
+img.src="img/macka3.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image4");
+img.src="img/macka3.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+
+
+var img = document.getElementById("image5");
+img.src="img/macka3.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+
+}
+
+
+
+function sakrij(elem, elem1) {
+    document.getElementById(elem).style.display="none";
+    document.getElementById(elem1).style.display="none";
+}
+
+
+function prikazi(elem, elem1){
+    document.getElementById(elem).style.display="block";
+    document.getElementById(elem1).style.display="block";
+}
