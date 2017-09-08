b7a0afefe0904692a00fa494e777844c3aaa43ef
littleWanderer <teateafb.tm@gmail.com>
2017-09-08 05:24:35 +0200

    Sidebar changes added; Minor changes on about us page added

--- js/board.js
+++ js/board.js
@@ -3,51 +3,67 @@
 
 function myFunction()
 {
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2016/17";;
+
+
+
 var img = document.getElementById("image");
 img.src="img/macka.jpg";
-document.getElementById("ime").innerHTML = "IME I PREZIME";
-document.getElementById("pozicija").innerHTML = "POZICIJA";
+document.getElementById("ime").innerHTML = "JELENA JAKŠIĆ";
+document.getElementById("pozicija").innerHTML = "PR koordinator";
+
+document.getElementById("a_li_n").setAttribute("href", "https://www.facebook.com"); //ovo ide kod svakog
+document.getElementById("a_li_k").setAttribute("href", "https://www.facebook.com"); 
+
 
 
 var img = document.getElementById("image1");
 img.src="img/macka.jpg";
-document.getElementById("ime1").innerHTML = "IME I PREZIME";
-document.getElementById("pozicija1").innerHTML = "POZICIJA";
+document.getElementById("ime1").innerHTML = "JOVAN TRTICA";
+document.getElementById("pozicija1").innerHTML = "Predsednik";
+
+document.getElementById("a_li_t").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_m").setAttribute("href", "https://www.facebook.com"); 
+
 
 
 var img = document.getElementById("image2");
 img.src="img/macka.jpg";
-document.getElementById("ime2").innerHTML = "IME I PREZIME";
-document.getElementById("pozicija2").innerHTML = "POZICIJA";
+document.getElementById("ime2").innerHTML = "ANJA ANŽEL";
+document.getElementById("pozicija2").innerHTML = "HR koordinator";
 
 
 var img = document.getElementById("image3");
 img.src="img/macka.jpg";
-document.getElementById("ime3").innerHTML = "IME I PREZIME";
-document.getElementById("pozicija3").innerHTML = "POZICIJA";
+document.getElementById("ime3").innerHTML = "PREDRAG TERZIĆ";
+document.getElementById("pozicija3").innerHTML = "FR koordinator";
 
 
 var img = document.getElementById("image4");
 img.src="img/macka.jpg";
-document.getElementById("ime4").innerHTML = "IME I PREZIME";
-document.getElementById("pozicija4").innerHTML = "POZICIJA";
+document.getElementById("ime4").innerHTML = "OGNJEN LUKAČEVIĆ";
+document.getElementById("pozicija4").innerHTML = "Sekretar";
 
 
 var img = document.getElementById("image5");
 img.src="img/macka.jpg";
-document.getElementById("ime5").innerHTML = "IME I PREZIME";
-document.getElementById("pozicija5").innerHTML = "POZICIJA";
+document.getElementById("ime5").innerHTML = "KATARINA JOJIĆ";
+document.getElementById("pozicija5").innerHTML = "Blagajnik";
 
 }
 
 
 
 
-/* ---------------   BOARD 15/16 ----------*/
+/* ---------------   BOARD 15/16 ---------- */
 
 
 function myFunction1()
 {
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2015/16";;
+	
 var img = document.getElementById("image");
 img.src="img/macka1.jpg";
 document.getElementById("ime").innerHTML = "IME I PREZIME";
@@ -87,11 +103,15 @@ document.getElementById("pozicija5").innerHTML = "POZICIJA";
 
 
 
-/* ---------------   BOARD 14/15 ----------*/
+/* ---------------   BOARD 14/15 ---------- */
 
 
 function myFunction2()
 {
+
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2014/15";;
+
 var img = document.getElementById("image");
 img.src="img/macka2.jpg";
 document.getElementById("ime").innerHTML = "IME I PREZIME";
@@ -129,11 +149,14 @@ document.getElementById("pozicija5").innerHTML = "POZICIJA";
 }
 
 
-/* ---------------   BOARD 13/14 ----------*/
+/* ---------------   BOARD 13/14 ---------- */
 
 
 function myFunction3()
 {
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2013/14";
+
 var img = document.getElementById("image");
 img.src="img/macka3.jpg";
 document.getElementById("ime").innerHTML = "IME I PREZIME";
