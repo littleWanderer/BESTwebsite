b2de4306b283dbab1fe886fb6f919d1590c2e45e
littleWanderer <teateafb.tm@gmail.com>
2017-10-14 01:51:09 +0200

    Sonja

---
+++ s/js/board.js
@@ -0,0 +1,269 @@
+/* ---------------   BOARD 16/17 ----------*/
+
+
+function myFunction()
+{
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2016/17";;
+
+
+
+var img = document.getElementById("image");
+img.src="img/macka.jpg";
+document.getElementById("ime").innerHTML = "JELENA JAKŠIĆ";
+document.getElementById("pozicija").innerHTML = "PR koordinator";
+document.getElementById("a_li_n").setAttribute("href", "https://www.facebook.com"); //ovo ide kod svakog
+document.getElementById("a_li_k").setAttribute("href", "https://www.facebook.com"); 
+
+
+
+var img = document.getElementById("image1");
+img.src="img/macka.jpg";
+document.getElementById("ime1").innerHTML = "JOVAN TRTICA";
+document.getElementById("pozicija1").innerHTML = "Predsednik";
+document.getElementById("a_li_t").setAttribute("href", "https://www.google.com"); 
+document.getElementById("a_li_m").setAttribute("href", "https://www.facebook.com"); 
+
+
+var img = document.getElementById("image2");
+img.src="img/macka.jpg";
+document.getElementById("ime2").innerHTML = "ANJA ANŽEL";
+document.getElementById("pozicija2").innerHTML = "HR koordinator";
+document.getElementById("a_li_q").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_w").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image3");
+img.src="img/macka.jpg";
+document.getElementById("ime3").innerHTML = "PREDRAG TERZIĆ";
+document.getElementById("pozicija3").innerHTML = "FR koordinator";
+document.getElementById("a_li_a").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_s").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image4");
+img.src="img/macka.jpg";
+document.getElementById("ime4").innerHTML = "OGNJEN LUKAČEVIĆ";
+document.getElementById("pozicija4").innerHTML = "Sekretar";
+document.getElementById("a_li_e").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_r").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image5");
+img.src="img/macka.jpg";
+document.getElementById("ime5").innerHTML = "KATARINA JOJIĆ";
+document.getElementById("pozicija5").innerHTML = "Blagajnik";
+document.getElementById("a_li_j").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_l").setAttribute("href", "https://www.facebook.com");
+}
+
+
+
+
+/* ---------------   BOARD 15/16 ---------- */
+
+
+function myFunction1()
+{
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2015/16";;
+
+
+	
+var img = document.getElementById("image");
+img.src="img/macka1.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+document.getElementById("a_li_n").setAttribute("href", "https://www.google.com"); //ovo ide kod svakog
+document.getElementById("a_li_k").setAttribute("href", "https://www.facebook.com");
+
+
+var img = document.getElementById("image1");
+img.src="img/macka1.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+document.getElementById("a_li_t").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_m").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image2");
+img.src="img/macka1.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+document.getElementById("a_li_q").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_w").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image3");
+img.src="img/macka1.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+document.getElementById("a_li_a").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_s").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image4");
+img.src="img/macka1.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+document.getElementById("a_li_e").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_r").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image5");
+img.src="img/macka1.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+document.getElementById("a_li_j").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_l").setAttribute("href", "https://www.facebook.com");
+}
+
+
+
+
+/* ---------------   BOARD 14/15 ---------- */
+
+
+function myFunction2()
+{
+
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2014/15";;
+
+
+
+var img = document.getElementById("image");
+img.src="img/macka2.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+document.getElementById("a_li_n").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_k").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image1");
+img.src="img/macka2.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+document.getElementById("a_li_t").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_m").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image2");
+img.src="img/macka2.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+document.getElementById("a_li_q").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_w").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image3");
+img.src="img/macka2.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+document.getElementById("a_li_a").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_s").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image4");
+img.src="img/macka2.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+document.getElementById("a_li_e").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_r").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image5");
+img.src="img/macka2.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+document.getElementById("a_li_j").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_l").setAttribute("href", "https://www.facebook.com");
+}
+
+
+/* ---------------   BOARD 13/14 ---------- */
+
+
+function myFunction3()
+{
+
+document.getElementById("io").innerHTML = "IZVRŠNI ODBOR 2013/14";
+
+
+
+var img = document.getElementById("image");
+img.src="img/macka3.jpg";
+document.getElementById("ime").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija").innerHTML = "POZICIJA";
+document.getElementById("a_li_n").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_k").setAttribute("href", "https://www.facebook.com");
+
+
+var img = document.getElementById("image1");
+img.src="img/macka3.jpg";
+document.getElementById("ime1").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija1").innerHTML = "POZICIJA";
+document.getElementById("a_li_t").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_m").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image2");
+img.src="img/macka3.jpg";
+document.getElementById("ime2").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija2").innerHTML = "POZICIJA";
+document.getElementById("a_li_q").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_w").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image3");
+img.src="img/macka3.jpg";
+document.getElementById("ime3").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija3").innerHTML = "POZICIJA";
+document.getElementById("a_li_a").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_s").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image4");
+img.src="img/macka3.jpg";
+document.getElementById("ime4").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija4").innerHTML = "POZICIJA";
+document.getElementById("a_li_e").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_r").setAttribute("href", "https://www.facebook.com");
+
+
+
+var img = document.getElementById("image5");
+img.src="img/macka3.jpg";
+document.getElementById("ime5").innerHTML = "IME I PREZIME";
+document.getElementById("pozicija5").innerHTML = "POZICIJA";
+document.getElementById("a_li_j").setAttribute("href", "https://www.facebook.com"); 
+document.getElementById("a_li_l").setAttribute("href", "https://www.facebook.com");
+}
+
+
+
+function sakrij(elem, elem1) {
+    document.getElementById(elem).style.display="none";
+    document.getElementById(elem1).style.display="none";
+}
+
+
+function prikazi(elem, elem1){
+    document.getElementById(elem).style.display="block";
+    document.getElementById(elem1).style.display="block";
+}
